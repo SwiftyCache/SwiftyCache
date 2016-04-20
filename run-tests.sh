74f47f45bc49ea3fe7a12019aeb8894bfb2fcf7a
Haoming Ma <brightpony@gmail.com>
2016-04-20 23:44:29 +1200

    Removed the iPhone 4s destination in the xcodebuild command to run the iOS tests.

--- run-tests.sh
+++ run-tests.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-xcodebuild clean test -workspace SwiftyCache.xcworkspace -scheme SwiftyCache-iOS -destination "platform=iOS Simulator,name=iPhone 4s" -destination "platform=iOS Simulator,name=iPhone 6 Plus" -enableCodeCoverage YES
+xcodebuild clean test -workspace SwiftyCache.xcworkspace -scheme SwiftyCache-iOS -destination "platform=iOS Simulator,name=iPhone 6 Plus" -enableCodeCoverage YES
 xcodebuild clean test -workspace SwiftyCache.xcworkspace -scheme SwiftyCache-OSX -sdk macosx -enableCodeCoverage YES
 xcodebuild clean test -workspace SwiftyCache.xcworkspace -scheme SwiftyCache-tvOS -destination "platform=tvOS Simulator,name=Apple TV 1080p" -enableCodeCoverage YES
 
